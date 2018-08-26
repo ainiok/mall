59615275d35f059ee91e5c98291c741244406551
xiao <jx_xiaojin@aliyun.com>
2018-08-26 23:32:45 +0800

    init

---
+++ wxapp/pages/component/index.js
@@ -0,0 +1,72 @@
+// pages/component/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banner:[
+      '/images/banner/b1.jpg',
+      '/images/banner/b2.jpg',
+      '/images/banner/b3.jpg'
+    ],
+    autoplay:true,
+    interval:3000
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
